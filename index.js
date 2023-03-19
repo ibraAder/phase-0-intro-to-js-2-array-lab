4298c703ed77b3307e9fffef069dfa858ab3273a
ibraAder <127183391+ibraAder@users.noreply.github.com>
2023-03-19 22:56:11 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+cats.length=0;
+cats.push("Ralph");
+cats.unshift["Bob"];
+cats.pop["Garfield"];
+cats.shift["Milo"];
+const allcats=[...cats,"Broom"];
+const precats=["Arnold",...cats];
+cats.slice[2];
+cats.slice[0];
+
+
+
+
+
